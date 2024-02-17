277ee24d8747ae3a3e1540f40cc26c212a8d3b06
oriastanjung <oriastan999@gmail.com>
2024-02-18 00:40:31 +0700

    fix bug addRoundKey

--- enkripsiPresent.js
+++ enkripsiPresent.js
@@ -89,7 +89,7 @@ function encryption(plainteks, rounds){
 }
 
 // const inputHex = konversiStringKeHeksadesimal("0000000000000000") // 64 bit atau 8 karakter atau 16 heksa
-const inputHex = konversiStringKeHeksadesimal("49544b4552454e37") // 64 bit atau 8 karakter atau 16 heksa (ITKEREN7_)
+const inputHex = konversiStringKeHeksadesimal("49544b4552454e37") // 64 bit atau 8 karakter atau 16 heksa (ITKEREN7)
 const cipherteks = encryption(inputHex, 32)
 console.log("\ncipherteks >>> ", cipherteks)
 
